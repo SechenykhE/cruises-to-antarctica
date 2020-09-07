4146eb9bb5ddce903383bc628de8763ed32924ba
Elena Sechenykh <sechenyh@inbox.ru>
2020-09-07 10:12:29 +0300

    Добавляет подвал страницы

--- build/js/script.js
+++ build/js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var headerMain = document.querySelector(".header-nav");
-var button = document.querySelector(".header-nav button");
+var headerMain = document.querySelector('.header-nav');
+var button = document.querySelector('.header-nav button');
 
-headerMain.classList.remove("header-nav--nojs");
+headerMain.classList.remove('header-nav--nojs');
 
-button.addEventListener("click", function() {
-  if (headerMain.classList.contains("header-nav--closed")) {
-    headerMain.classList.remove("header-nav--closed");
-    headerMain.classList.add("header-nav--opened");
+button.addEventListener('click', function () {
+  if (headerMain.classList.contains('header-nav--closed')) {
+    headerMain.classList.remove('header-nav--closed');
+    headerMain.classList.add('header-nav--opened');
   } else {
-    headerMain.classList.add("header-nav--closed");
-    headerMain.classList.remove("header-nav--opened");
+    headerMain.classList.add('header-nav--closed');
+    headerMain.classList.remove('header-nav--opened');
   }
 });
