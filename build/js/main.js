6aa1b0d9949cc93185e71493b4c659e0fc51592b
Elena Sechenykh <sechenyh@inbox.ru>
2020-09-15 15:14:55 +0300

    Исправляет баги повторного тестирования

--- build/js/main.js
+++ build/js/main.js
@@ -3,6 +3,7 @@
 (function () {
   var headerMain = document.querySelector('.header-nav');
   var button = document.querySelector('.header-nav button');
+  var body = document.querySelector('body');
 
   headerMain.classList.remove('header-nav--nojs');
 
@@ -10,9 +11,11 @@
     if (headerMain.classList.contains('header-nav--closed')) {
       headerMain.classList.remove('header-nav--closed');
       headerMain.classList.add('header-nav--opened');
+      body.classList.add('hidden');
     } else {
       headerMain.classList.add('header-nav--closed');
       headerMain.classList.remove('header-nav--opened');
+      body.classList.remove('hidden');
     }
   });
 
